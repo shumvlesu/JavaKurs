adc4bd17751b8c8a8bb239b7218a80741cc0d978
shumvlesu <sashumihin@gmail.com>
2019-03-22 12:09:10 +0300

    Thread

--- src/Trends.java
+++ src/Trends.java
@@ -1,20 +1,34 @@
+//Два примера создания потоков
 public class Trends {
 
   public static void main(String[] args) {
+    //Первый метод создания потока
     MyTread potok1 = new MyTread();
     potok1.start();
 
+    //Второй метод создания потока
+    Thread potok2 = new Thread(new Runner());
+    potok2.start();
+
+    //Выводим сообщение из основного потока, который main
     System.out.println("Thread there");
 
   }
+}
 
-
+class Runner implements Runnable {
+  @Override
+  public void run() {
+    for (int i = 0; i < 1000; i++) {
+      System.out.println("Thread potok 2 -" + i);
+    }
+  }
 }
 
 class MyTread extends Thread {
   public void run() {
     for (int i = 0; i < 1000; i++) {
-      System.out.println("Thread " + i);
+      System.out.println("Thread potok 1 -" + i);
     }
   }
 
